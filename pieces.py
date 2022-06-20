f0d5f67f48346e01d2ac29e66e233133900863e3
j1642 <60148902+j1642@users.noreply.github.com>
2022-06-20 17:52:16 -0400

    Add castling and queen and king tests

--- pieces.py
+++ pieces.py
@@ -1,5 +1,8 @@
 #import board
 
+# TODO: use closure (e.g. update_all_moves() ) in game.py (I think) to
+# hold a_file, h_file, etc. instead of each piece creating their own.
+
 class Pawn:
     def __init__(self, name: str, white_or_black: str, position: int):
         self.name = name
@@ -219,10 +222,25 @@ class Rook:
                     
         self.moves = sorted(all_moves)
     
-    def move_piece(self, new_square: int):
-        if new_square in self.moves:
-            old_square = self.square
-            self.square = new_square
+    def move_piece(self, new_square: int, castling=False):
+        # TODO: update board list
+        if castling:
+            if not self.has_moved:
+                # Do not check if new_square is in self.moves
+                if self.color == 'white' and new_square in (3, 5):
+                    old_square, self.square = self.square, new_square
+                    self.has_moved = True
+                elif self.color == 'black' and new_square in (59, 61):
+                    old_square, self.square = self.square, new_square
+                    self.has_moved = True
+                else:
+                    raise Exception('Rook', rook.name, 'cannot castle to', new_square)
+            else:
+                raise Exception(f'Castling rook "{self.name}" is illegal. ' \
+                                f'Rook "{self.name}" has already moved.')
+            
+        elif new_square in self.moves:
+            old_square, self.square = self.square, new_square
             self.has_moved = True
     #        squares[old_square], squares[new_square] = '', self.name
         else:
@@ -251,15 +269,20 @@ class Queen:
     # Copied from bishop and rook update_moves()
     def update_moves(self):
         all_moves = []
-        # Prevent going over side of board.
-        # Switch these with board_obj attributes later on.
         a_file = list(range(0, 57, 8))
         h_file = list(range(7, 64, 8))
         # Vertical and horizontal moves
-        for vert_horiz_scalar in range(1, 7):
-            for direction in (-8, -1, 1, 8):
-                if 0 <= (self.square + direction * vert_horiz_scalar) <= 63:
-                    all_moves.append(self.square + direction * vert_horiz_scalar)
+        for direction in (-8, -1, 1, 8):
+            for vert_horiz_scalar in range(1, 8):
+                new_square = self.square + direction * vert_horiz_scalar
+                if 0 <= new_square <= 63:
+                    all_moves.append(new_square)
+                    if new_square in a_file or new_square in h_file:
+                        # Prevent crossing board sides.
+                        break
+                # Prevent move calculations past top or bottom of board.
+                else:
+                    break
         # Diagonal moves
         for direction in (-9, -7, 7, 9):
             for diagonal_scalar in range(1, 7):
@@ -267,7 +290,7 @@ class Queen:
                 if 0 <= new_square <= 63:
                     all_moves.append(self.square + direction * diagonal_scalar)
                     # Do not allow piece to move over the side of the board
-                    if new_square in a_file or h_file:
+                    if new_square in a_file or new_square in h_file:
                         break
                 # Prevent useless calculation.
                 else:
@@ -306,48 +329,76 @@ class King:
     # TODO: Remove enemy-controlled squares
     def update_moves(self):
         all_moves = []
-        for delta in (7, 8, 9, -1, 1, -9, -8, -7):
-            if self.square + delta >= 0:
-                all_moves.append(self.square + delta)         
+        a_file = list(range(0, 57, 8))
+        h_file = list(range(7, 64, 8))
+        first_rank = list(range(0, 8))
+        eighth_rank = list(range(56, 64))
+        directions = [7, 8, 9, -1, 1, -9, -8, -7]
+        if self.square in first_rank:
+            directions = directions[:5]
+        elif self.square in eighth_rank:
+            directions = directions[3:]
+        # Separate if/elif for ranks (rows) and files (columns).
+        # A piece can be in both the first rank and the H file.
+        if self.square in a_file:
+            for direction in (7, -1, -9):
+                try:
+                    directions.remove(direction)
+                except ValueError:
+                    continue
+        elif self.square in h_file:
+            for direction in (9, 1, -7):
+                try:
+                    directions.remove(direction)
+                except ValueError:
+                    continue
+            
+        for delta in directions:
+            new_square = self.square + delta
+            if  -1 < new_square < 64:
+                all_moves.append(self.square + delta)
         
         # TODO: Finish Castling - get rook to move as well
+        # TODO: An empty square that is under attack b/w rook and king
+        # prevents castling?
         if self.has_moved is False:
-            if self.color == 'white':
-                # Castle kingside
-                if board.white_rk.has_moved is False \
+            if self.color == 'white' and self.square == 4:
+                # Check if castling kingside is possible.
+                if white_rook_h.has_moved is False \
                     and board.squares[5] == board.squares[6] == ' ':
                         all_moves.append(6)
-                # Castle queenside
-                if board.white_rq.has_moved is False \
+                # Check if castling queenside is possible.
+                if white_rook_a.has_moved is False \
                     and board.squares[1] == board.squares[2] == ' ':
                         all_moves.append(2)
-            elif self.color == 'black':
+            elif self.color == 'black' and self.square == 60:
                 # Castle kingside
-                if board.black_rk.has_moved is False \
+                if black_rook_h.has_moved is False \
                     and board.squares[61] == board.squares[62] == ' ':
                         all_moves.append(62)
                 # Castle queenside
-                if board.black_rq.has_moved is False \
+                if black_rook_a.has_moved is False \
                     and board.squares[57] == board.squares[58] == ' ':
                         all_moves.append(58)
                         
-        # Remove illegal king moves into opponent controlled squares
-        all_moves_set = set(all_moves)
-        all_moves = list(all_moves_set)
-        
-        if self.color == 'white':
-            for illegal_move in board.black_controlled_squares():
-                try:
-                    all_moves.remove(illegal_move)
-                except ValueError:
-                    pass
+        # Remove illegal king moves into opponent controlled squares.
+        # King cannot willingly move into check.
+        #all_moves_set = set(all_moves)
+        #all_moves = list(all_moves_set)
+       # 
+        #if self.color == 'white':
+         #   for illegal_move in board.black_controlled_squares():
+          #      try:
+           #         all_moves.remove(illegal_move)
+            #    except ValueError:
+             #       continue
                 
-        elif self.color == 'black':
-            for illegal_move in board.white_controlled_squares():
-                try:
-                    all_moves.remove(illegal_move)
-                except ValueError:
-                    pass
+       # elif self.color == 'black':
+        #    for illegal_move in board.white_controlled_squares():
+         #       try:
+          #          all_moves.remove(illegal_move)
+           #     except ValueError:
+            #        continue
                 
         self.moves = all_moves
     
@@ -355,6 +406,19 @@ class King:
         if new_square in self.moves:
             old_square = self.square
             self.square = new_square
+            # Check if king is castling.
+            if self.has_moved is False:
+                if self.color == 'white':
+                    if new_square == 2:
+                        white_rook_a.move_piece(3, castling=True)
+                    elif new_square == 6:
+                        white_rook_h.move_piece(5, castling=True)
+                elif self.color == 'black':
+                    if new_square == 58:
+                        black_rook_a.move_piece(59, castling=True)
+                    elif new_square == 62:
+                        black_rook_h.move_piece(61, castling=True)
+                        
             self.has_moved = True
    #         squares[old_square], squares[new_square] = '', self.name
         else:
@@ -370,10 +434,21 @@ class King:
 if __name__ == '__main__':
     import unittest
     
+    # For king testing. Global variables
+    class Board:
+        def __init__(self):
+            self.squares = [' '] * 64
+    board = Board()
+    black_rook_a = Rook('ra', 'black', 56)
+    black_rook_h = Rook('rh', 'black', 63)
+    white_rook_a = Rook('Ra', 'white', 0)
+    white_rook_h = Rook('Rh', 'white', 7)
+    
     class TestPieceMovement(unittest.TestCase):
         def test_pawn_movement(self):
             pawn = Pawn('Pa', 'white', 8)
             self.assertFalse(pawn.moves)
+            self.assertFalse(pawn.has_moved)
             pawn.update_moves()
             self.assertEqual(pawn.moves, [16, 24])
             pawn.move_piece(24)
@@ -416,11 +491,58 @@ if __name__ == '__main__':
         def test_rook_movement(self):
             rook = Rook('Ra', 'white', 9)
             self.assertFalse(rook.moves)
+            self.assertFalse(rook.has_moved)
             rook.update_moves()
             supposed_rook_moves = [1, 8] + list(range(17, 58, 8)) \
                                 + list(range(10, 16))
             self.assertEqual(set(rook.moves), set(supposed_rook_moves))
+            rook.move_piece(10)
+            self.assertTrue(rook.has_moved)
+            
+            rook = Rook('Ra', 'white', 0)
+            self.assertFalse(rook.moves)
+            rook.move_piece(3, castling=True)
+            self.assertEqual(rook.square, 3)
+            self.assertTrue(rook.has_moved)
+            
+            rook = Rook('ra', 'black', 56)
+            self.assertFalse(rook.moves)
+            rook.move_piece(59, castling=True)
+            self.assertEqual(rook.square, 59)
+            self.assertTrue(rook.has_moved)
+            self.assertRaises(Exception, rook.move_piece, 59, castling=True)
+            
+            rook = Rook('ra', 'white', 0)
+            self.assertRaises(Exception, rook.move_piece, 100, castling=True)
             
             
+        def test_queen_movement(self):
+            queen = Queen('Q', 'white', 9)
+            self.assertFalse(queen.moves)
+            queen.update_moves()
+            # Possible rook moves plus possible bishop moves.
+            supposed_queen_moves = [1, 8] + list(range(17, 58, 8)) \
+                                + list(range(10, 16))
+            supposed_queen_moves += [0, 16, 2, 18, 27, 36, 45, 54, 63]
+            self.assertEqual(set(queen.moves), set(supposed_queen_moves))
+            
+        def test_king_movement(self):
+            
+            king = King('k', 'black', 63)
+            self.assertFalse(king.has_moved)
+            self.assertFalse(king.moves)
+            # Rooks need to exist to check for castling possibility.
+            board = Board()
+            king.update_moves()
+            self.assertEqual(set(king.moves), set((62, 55, 54)))
+            king.move_piece(55)
+            self.assertTrue(king.has_moved)
+        
+            king = King('K', 'white', 4)
+            self.assertFalse(king.moves)
+            self.assertFalse(king.has_moved)
+            king.update_moves()
+            self.assertEqual(set(king.moves), set((3, 11, 12, 13, 5, 2, 6)))
+
             
     unittest.main()

a89fbd3f8a611af81c0aa5eb6d0ce9a3bba33672
j1642 <60148902+j1642@users.noreply.github.com>
2022-07-28 12:03:17 -0400

    Fix and simplify print_fen_to_terminal function.

--- chess_utilities.py
+++ chess_utilities.py
@@ -7,34 +7,46 @@ board.Board.squares list for testing.
 '''
 import copy
 #from time import gmtime, strftime
+import board
+import pieces
 
 # TODO: expand this for saving game state using a 'Save' button?
 
-def fen_to_terminal_display(fen_string):
-    '''Prints a FEN string into the terminal in the board format.
+def print_fen_to_terminal(fen_string):
+    '''Prints a FEN string into the terminal as an ASCII chess board.
     Similar to board.Board.__repr__.
     '''
     fen_ls = fen_string.split(' ')
     fen_ls[0] = fen_ls[0].split('/')
+    assert len(fen_ls[0]) == 8
     print(fen_ls)
 
     return_ls = []
 
-    for i in range(len(fen_ls[0][0])):
-        if fen_ls[0][i].isalpha():
-            return_ls.append('|'+ '|'.join(fen_ls[0][i]) + '|')
-        elif fen_ls[0][i].isnumeric():
-            return_ls.append('| ' * int(fen_ls[0][i]) + '|')
-        elif fen_ls[0][i].isalnum():
-            row_str = '|'
-            for char in fen_ls[0][i]:
-                if char.isalpha():
-                    row_str += char + '|'
-                elif char.isnumeric():
-                    row_str += ' |' * int(char)
-            return_ls.append(row_str)
+    #for i in range(len(fen_ls[0][0])):
+     #   if fen_ls[0][i].isalpha():
+      #      return_ls.append('|'+ '|'.join(fen_ls[0][i]) + '|')
+       # elif fen_ls[0][i].isnumeric():
+        #    return_ls.append('| ' * int(fen_ls[0][i]) + '|')
+        #elif fen_ls[0][i].isalnum():
+         #   row_str = '|'
+          #  for char in fen_ls[0][i]:
+           #     if char.isalpha():
+            #        row_str += char + '|'
+             #   elif char.isnumeric():
+              #      row_str += ' |' * int(char)
+            #return_ls.append(row_str)
 
-    #return return_ls
+    for row in fen_ls[0]:
+        row_out = ['|']
+        for char in row:
+            if char.isalpha():
+                row_out.append(char)
+                row_out.append('|')
+            elif char.isdigit():
+                row_out.append(' |' * int(char))
+        return_ls.append(''.join(row_out))
+
     for row in return_ls:
         print(row)
 
@@ -46,11 +58,11 @@ def import_fen_to_board(fen: str):
 
     turn_to_move = fen.pop()
     if turn_to_move == 'w':
-        turn_to_move = 'white'
+        last_move_color = 'black'
     elif turn_to_move == 'b':
-        turn_to_move = 'black'
+        last_move_color = 'white'
 
-    chessboard.last_move_piece = pieces.Pawn('placeholder', turn_to_move, 100)
+    chessboard.last_move_piece = pieces.Pawn('placeholder', last_move_color, 100)
 
     if fen[-1] == ' ':
         fen.pop()
@@ -74,6 +86,14 @@ def import_fen_to_board(fen: str):
                     piece_color = 'black'
                 piece = letter_to_piece[char.lower()](char, piece_color, squares_ind_counter)
                 chessboard.squares[squares_ind_counter] = piece
+                if piece.color == 'white':
+                    chessboard.white_pieces.append(piece)
+                    if isinstance(piece, pieces.King):
+                        chessboard.white_king = piece
+                elif piece.color == 'black':
+                    chessboard.black_pieces.append(piece)
+                    if isinstance(piece, pieces.King):
+                        chessboard.black_king = piece
                 squares_ind_counter += 1
 
             elif char.isdigit():
@@ -100,10 +120,10 @@ def export_board_to_fen(chessboard):
 
     fen = list(reversed(fen))
     # Add piece positions to fen
-    for ind, sublist in enumerate(fen):
+    for ind, row in enumerate(fen):
         partial_fen = []
         adjacent_empty_squares_count = 0
-        for square in sublist:
+        for square in row:
             if square == ' ':
                 adjacent_empty_squares_count += 1
             else:
@@ -134,8 +154,6 @@ def export_board_to_fen(chessboard):
 
     fen = ' '.join([fen, white_or_black_to_move])
 
-
-
     #with open('FEN_exports.txt', 'a') as export:
      #   current_time = strftime('%Y-%m-%d %H:%M', gmtime())
       #  export.write(f'{current_time}\n')
@@ -146,8 +164,6 @@ def export_board_to_fen(chessboard):
 
 if __name__ == '__main__':
     import unittest
-    import board
-    import pieces
 
     class TestChessUtilities(unittest.TestCase):
         def test_export_board_to_fen(self):
@@ -173,6 +189,7 @@ if __name__ == '__main__':
 
             # print(chessboard)
             self.assertIsInstance(chessboard.squares[0], pieces.Rook)
+            self.assertEqual(chessboard.squares[0].color, 'white')
 
 
 
